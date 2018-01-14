01240d865b310b1c2fb8a1fca8c3458780b31c3a
e533611 <35010403+e533611@users.noreply.github.com>
2018-01-14 22:58:38 +0100

    shoot pict

--- TestTab(1)/app/src/main/java/com/example/eli/testtab/NewUserActivity.java
+++ TestTab(1)/app/src/main/java/com/example/eli/testtab/NewUserActivity.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,15 +29,15 @@ import java.util.TimeZone;
 
 public class NewUserActivity extends AppCompatActivity {
 
-    Context c;
     Usuario miUser;
     TextView nombre, password, email;
     EditText newname,newpass, newemail;
-    ImageButton newfoto;
+    ImageButton newFoto;
     int id_usr;
     String resultat;
     GlobalState gs;
-
+    ImageView foto;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,10 +54,22 @@ public class NewUserActivity extends AppCompatActivity {
         newpass= (EditText) findViewById(R.id.pwnu);
         newemail= (EditText) findViewById(R.id.emailnu);
 
-        newfoto = (ImageButton) findViewById(R.id.foto);
+        newFoto = (ImageButton) findViewById(R.id.foto);
 
         newpass.setText(getIntent().getStringExtra("password"));
         newemail.setText(getIntent().getStringExtra("email"));
+        newFoto.setOnClickListener(new View.OnClickListener()   {
+            public void onClick(View v)  {
+                try {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -66,7 +81,15 @@ public class NewUserActivity extends AppCompatActivity {
             }
         });
     }
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            foto.setImageBitmap(imageBitmap);
+            newFoto.setImageBitmap(imageBitmap);
+        }
+    }
 
     public class Descarga extends AsyncTask<String, Integer, String> {
         String Uname = newname.getText().toString();
